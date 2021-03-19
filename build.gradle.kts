4efe86b72e22e541c340d10140b45a130b0b012c
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2021-03-19 14:06:20 +0100

    Add maven publish task (to MavenCentral)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.4.31"
+    signing
+    `maven-publish`
 }
 
 group = "io.datalbry.testcontainers"
@@ -11,6 +13,63 @@ repositories {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+configure<SigningExtension> {
+    sign(configurations.archives.get())
+}
+
+configure<PublishingExtension> {
+    publications {
+        repositories {
+            maven {
+                name = "MavenCentral"
+                url = if (project.rootProject.version.toString().endsWith("SNAPSHOT")) {
+                    uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                } else {
+                    uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                }
+                credentials {
+                    username = project.findProperty("maven.central.username") as String
+                    password = project.findProperty("maven.central.password") as String
+                }
+            }
+        }
+        create<MavenPublication>("jar") {
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("Precise - ${project.name}")
+                description.set("Precise is a easy to use schema framework")
+                url.set("https://github.com/datalbry/testcontainer-bigtable")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("datalbry")
+                        name.set("DataLbry")
+                        email.set("devops@datalbry.io")
+                    }
+                }
+                scm {
+                    connection.set("https://github.com/datalbry/testcontainer-bigtable.git")
+                    developerConnection.set("scm:git:ssh:git@github.com:datalbry/testcontainer-bigtable.git")
+                    url.set("https://github.com/datalbry/testcontainer-bigtable")
+                }
+            }
+        }
+    }
 }
 
 dependencies {
