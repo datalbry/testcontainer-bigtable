84e4e03f3fa24392fba71f7eac8d57fd2fc452f8
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2021-03-22 17:40:50 +0100

    Fix container not running correctly

--- src/main/kotlin/io/datalbry/testcontainers/bigtable/BigTableContainer.kt
+++ src/main/kotlin/io/datalbry/testcontainers/bigtable/BigTableContainer.kt
@@ -1,6 +1,7 @@
 package io.datalbry.testcontainers.bigtable
 
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy
 
 /**
  * [BigTableContainer] is a simple testcontainer implementation based on the Google Cloud SDK.
@@ -9,11 +10,21 @@ import org.testcontainers.containers.GenericContainer
  * @author timo gruen - 2021-03-19
  */
 class BigTableContainer(
-    version: String = "latest"
+    version: String = "316.0.0-emulators",
+    private val port: Int = 8080
 )
     : GenericContainer<BigTableContainer>("gcr.io/google.com/cloudsdktool/cloud-sdk:$version")
 {
+
+    private val command = "gcloud beta emulators bigtable start --host-port 0.0.0.0:$port"
+
     init {
-        this.withCommand("gcloud beta emulators bigtable start")
+        this.withCommand("/bin/sh", "-c", command)
+        this.withExposedPorts(port)
+        this.setWaitStrategy(LogMessageWaitStrategy().withRegEx("(?s).*running.*$"));
+    }
+
+    fun getPort(): Int {
+        return getMappedPort(port)
     }
 }
